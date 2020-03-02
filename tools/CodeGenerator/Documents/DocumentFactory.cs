d85f646c9082bf891be4bf0734a12d0a7a83142c
Brad Marshall <bmarshall@sped.mobi>
2020-03-02 06:32:31 -0600

    Add project files.

---
+++ tools/CodeGenerator/Documents/DocumentFactory.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using Gisd.Sped.Schema;
+using Independentsoft.Office;
+using Independentsoft.Office.Word;
+
+namespace CodeGenerator.Documents
+{
+    internal static class DocumentFactory
+    {
+        public static WordDocument CreateDocument(Student student)
+        {
+            WordDocument doc = new WordDocument();
+
+            doc = doc.AddStudentInfo(student.Name, student.Id, student.Dob, student.Gender, student.Grade);
+
+            for (var i = 0; i < student.Goals.Count; i++)
+            {
+                if (i < 1)
+                {
+                    continue;
+                }
+
+                var goal = student.Goals[i];
+
+                doc = doc.AddGoal(goal);
+            }
+
+
+            return doc;
+        }
+
+        private static WordDocument AddGoal(this WordDocument source, Goal goal)
+        {
+            Paragraph p1 = new Paragraph();
+            Paragraph p2 = new Paragraph();
+
+            Run number = new Run();
+            number = number.Stylize(goal.Number, 20, "Arial", false, true, true);
+
+            Run colon = new Run();
+            colon.Stylize(":", 20, "Arial", false, true, false);
+
+            Run focus = new Run();
+            focus = focus.Stylize(goal.Focus, 20, "Arial", false, true, true);
+
+            Run annualGoalLabel = new Run();
+            annualGoalLabel = annualGoalLabel.Stylize("ANNUAL GOAL: ", 20, "Arial", false, true, false);
+
+            Run annualGoal = new Run();
+            annualGoal = annualGoal.Stylize(goal.AnnualGoal, 20, "Arial", italics: true, bold: true, underline: false);
+
+
+            p1.Add(number);
+            p1.Add(colon);
+            p1.Add(focus);
+            p2.Add(annualGoalLabel);
+            p2.Add(annualGoal);
+            p2.Add(new Run());
+
+            source.Body.Add(p1);
+            source.Body.Add(p2);
+
+            return source;
+        }
+
+
+
+        private static WordDocument AddStudentInfo(this WordDocument source, string name, string id, DateTimeOffset dob, Gender gender, string grade)
+        {
+            Paragraph p = new Paragraph();
+            Run title = new Run();
+            title = title.Stylize("Special Education Goal Data Collection Sheet", 24, "Arial", false, true, false);
+            p.Add(title);
+            p.Add(new Run());
+            Run info = new Run();
+            info = info.Stylize($"Name: {name}\tDOB: {dob:yyyy-MM-dd}\tID#: {id}\tGender: {gender}\tGrade: {grade}");
+            p.Add(info);
+            p.Add(new Run());
+            p.Add(new Run("Evaluator: _______________________________\tGrading Period: _______________________________"));
+            p.Add(new Run());
+            source.Body.Add(p);
+            return source;
+
+        }
+
+        private static Run Stylize(this Run source, string text, int fontSize = 20, string font = "Arial", bool italics = false, bool bold = false, bool underline = false)
+        {
+            source.AddText(text);
+            source.AsciiFont = font;
+            source.FontSize = fontSize;
+            if (bold)
+            {
+                source.Bold = ExtendedBoolean.True;
+            }
+
+            if (underline)
+            {
+                source.Underline = new Underline(UnderlinePattern.Single);
+            }
+
+            if (italics)
+            {
+                source.Italic = ExtendedBoolean.True;
+            }
+
+            return source;
+        }
+
+
+        public static void CreateDocuments(string jsonFilePath, string targetDirectory)
+        {
+            var collection = GetDataCollection(jsonFilePath);
+
+            foreach (var student in collection.Students)
+            {
+                var document = CreateDocument(student);
+
+                var fileName = "2019-2020_lchs_ale_marshall_gp5_" + student.Name.Replace(" ", "_").ToLower() + "_goal_documentation.docx";
+
+                var newPath = Path.Combine(targetDirectory, fileName);
+
+                document.Save(newPath, true);
+            }
+        }
+
+
+        private static DataCollection GetDataCollection(string jsonFilePath)
+        {
+            return DataCollection.FromJson(File.ReadAllText(jsonFilePath));
+        }
+    }
+}
---
+++ tools/CodeGenerator/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using Independentsoft.Office;
+using Independentsoft.Office.Word;
+
+namespace CodeGenerator
+{
+    public static class Program
+    {
+        private static StringBuilder Builder;
+        private static ClipboardWriter Writer;
+        public static void Main()
+        {
+            Before();
+
+            WordDocument doc = new WordDocument();
+
+            Run number = new Run();
+            number = number.Stylize("1", 20, "Arial", false, true, true);
+
+            Run colon = new Run();
+            colon.Stylize(": ", 20, "Arial", false, true, false);
+
+            Run focus = new Run();
+            focus = focus.Stylize("Applied Marketable Skills" + Environment.NewLine, 20, "Arial", false, true, true);
+
+
+
+            Paragraph p1 = new Paragraph();
+            p1.Add(number);
+            p1.Add(colon);
+            p1.Add(focus);
+
+
+            doc.Body.Add(p1);
+
+
+
+
+            Paragraph p2 = new Paragraph();
+            Run annualGoal = new Run();
+            annualGoal = annualGoal.Stylize("ANNUAL GOAL: ", 20, "Arial", false, true, false);
+            p2.Add(annualGoal);
+
+            Run goalText = new Run();
+            goalText = goalText.Stylize("From ARD year to the end of the 2019-2020 Academic year, when given verbal direct prompts, and verbal reminders to remain on tasks with open hands, Uriel will demonstrate attentiveness to self help activities independently with 70% accuracy.", 20, "Arial", true, true);
+
+            p2.Add(goalText);
+
+            doc.Body.Add(p2);
+
+            doc.Save("D:\\output.docx", true);
+
+
+            WriteLine();
+            Write("Press any key to continue...");
+            After();
+        }
+
+        private static Run Stylize(this Run source, string text, int fontSize = 20, string font = "Arial", bool italics = false, bool bold = false, bool underline = false)
+        {
+            source.AddText(text);
+            source.AsciiFont = font;
+            source.FontSize = fontSize;
+            if (bold)
+            {
+                source.Bold = ExtendedBoolean.True;
+            }
+
+            if (underline)
+            {
+                source.Underline = new Underline(UnderlinePattern.Single);
+            }
+
+            if (italics)
+            {
+                source.Italic = ExtendedBoolean.True;
+            }
+
+            return source;
+        }
+
+        private static void After()
+        {
+            Writer.Clip();
+            Console.WriteLine(Builder);
+            Console.ReadKey();
+        }
+
+        private static void Before()
+        {
+            Builder = new StringBuilder();
+            Writer = ClipboardWriter.CreateForStringBuilder(Builder);
+        }
+
+        private static void Write(string value)
+        {
+            Writer.Write(value);
+        }
+
+        private static void WriteLine(string value)
+        {
+            Writer.WriteLine(value);
+        }
+
+        private static void WriteLine()
+        {
+            Writer.WriteLine();
+        }
+
+        private static IDisposable OpenBlock()
+        {
+            return Writer.OpenBlock();
+        }
+    }
+}
